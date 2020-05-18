88c19cd9e549e242d6c180d67bf66e95caf69685
machun <machun@kedacom.com>
2020-05-18 20:18:06 +0800

    增加数字和列表测试代码

---
+++ 005_str().py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Project : python_learning
+# @File    : 005_str()
+# @Author  : Machun Michael
+# @Time    : 2020/5/18 19:22
+# @Software: PyCharm
---
+++ 006_列表.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Project : python_learning
+# @File    : 006_列表.py
+# @Author  : Machun Michael
+# @Time    : 2020/5/18 19:41
+# @Software: PyCharm
