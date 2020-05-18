9fe3ca23d04c6c4a1fd1bf2c45895e2453b2b7c4
machun <machun@kedacom.com>
2020-05-18 20:20:16 +0800

    增加数字和列表测试代码

--- 005_str().py
+++ 005_str().py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 # @Project : python_learning
-# @File    : 005_str()
+# @File    : 005_str().py
 # @Author  : Machun Michael
 # @Time    : 2020/5/18 19:22
 # @Software: PyCharm
+
+# str()，可以将非字符串类型转换为字符串
+
+name = "michael"
+age = 25
+addr = "shanghai, China"
+# age是int类型，不能直接使用，需要转换为字符串
+# introduce = "hello, my name's " + name + ". I am " + age + "years old, and I live in " + addr
+introduce = "hello, my name's " + name + ". I am " + str(age) + "years old, and I live in " + addr
+print(introduce)
--- 006_列表.py
+++ 006_列表.py
@@ -4,3 +4,39 @@
 # @Author  : Machun Michael
 # @Time    : 2020/5/18 19:41
 # @Software: PyCharm
+
+
+'''
+1. 列表：列表是一个有序集合，由一系列按照特定顺序排列的元素组成，类似于C语言中的数组
+   列表的元素可以是数字、字符、字符串等等
+   用"[]"来表示列表，用","来分隔列表元素
+
+2. 访问列表元素
+   类似于访问数组元素，可以使用 “列表名+索引” 的方式访问列表元素。
+   同样，索引是从0开始；特别之处是，python中的索引可以用复数，例如-1表示列表的最后一个元素，-2表示列表的倒数第2个元素
+   注意，索引不能超过列表长度
+
+3. 修改列表中元素的值
+   给列表元素赋一个新的值，就可以修改元素的值
+
+4. 在列表中增加元素
+'''
+
+names  = ["michae", "Tom", "Lucy", "Lily", "Sheldon"]
+ages   = [25, 24, 23, 25, 22]
+scores = ['A', 'B', 'A', 'A', 'C']
+print(names)
+print(ages)
+print(scores)
+
+# 访问列表元素
+message = names[0] + "is " + str(ages[0]) + " years old, and he got an " + scores[0]
+print(message)
+print(names[-1])
+# print(names[-8])  # 索引值超过列表长度
+
+# 修改列表中元素的值
+names[1] = "Tony"
+print(names)
+
+# 在列表中增加元素
