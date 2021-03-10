f9a18cf201a95ae14c3728c5e4075c3241e96859
machun <302806766@qq.com>
2021-03-10 19:36:17 +0800

    修改目录结构和文件名，提交复习代码

---
+++ python_review/00_hello.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Project : python_learning
+# @File    : 00_hello.py
+# @Author  : Machun Michael
+# @Time    : 2021/3/8 21:18
+# @Software: PyCharm
+
+print("hello world")
+print('hello again')
+
+message = "hello michael" # 定义一个变量message，不需要加变量类型
+print(message)
+
+message = 14              # 同一个变量，可以赋予不同类型的值
+print(message)
+
+message = 'a'
+print(message)
---
+++ python_review/01_string.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Project : python_learning
+# @File    : 01_string.py
+# @Author  : Machun Michael
+# @Time    : 2021/3/8 21:29
+# @Software: PyCharm
+
+# 字符串可以使用单引号，也可以使用双引号括起来
+# 可以灵活的在字符串中，使用引号和撇号
+
+name_1 = "michael"
+name_2 = 'tom'
+
+print(name_1)
+print(name_2)
+
+question = "What's your name?"
+print(question)
+
+answer = "My name's michael."
+print(answer)
